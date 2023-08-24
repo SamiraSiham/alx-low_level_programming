7f60285d11365c7ed79cec910b8190b5e64b8ead
Samira Siham <samirasiham.school2021@gmail.com>
2023-08-24 16:59:00 -0700

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,28 @@
-[200~#include "main.h"
+#include "main.h"
 
 /**
- *  * *leet - take strings and capitalize words
- *   * @str: string to capitalize
- *    * Return: capitalized words
- *     */
+ * *leet - take strings and capitalize words
+ * @str: string to capitalize
+ * Return: capitalized words
+ */
 char *leet(char *str)
 {
-		int i, j;
-			char encoded[] = "aAeEoOtTlL4433007711";
+	int i, j;
+	char encoded[] = "aAeEoOtTlL4433007711";
 
-				i = 0;
+	i = 0;
 
-					while (*(str + i) != '\0')
-							{
-										for (j = 0; encoded[j] != '\0'; j++)
-													{
-																	if (str[i] == encoded[j])
-																					{
-																										str[i] = encoded[j + 10];
-																														break;
-																																	}
-																			}
-												i++;
-													}
-						return (str);
+	while (*(str + i) != '\0')
+	{
+		for (j = 0; encoded[j] != '\0'; j++)
+		{
+			if (str[i] == encoded[j])
+			{
+				str[i] = encoded[j + 10];
+				break;
+			}
+		}
+		i++;
+	}
+	return (str);
 }
