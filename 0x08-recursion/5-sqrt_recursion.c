4e1ff72ae16e5857a7853e1c658f4c0a2dc59260
Samira Siham <samirasiham.school2021@gmail.com>
2023-08-30 18:12:34 -0700

    done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,7 @@
 int square(int n, int val);
 int _sqrt_recursion(int n)
 {
-return (square(n,1));
+return (square(n, 1));
 }
 /**
  * square - find square root
@@ -18,9 +18,9 @@ return (square(n,1));
  */
 int square(int n, int val)
 {
-if(val * val == n)
+if (val * val == n)
 	return (val);
-else if (val * val  < n )
+else if (val * val  < n)
 	return (square(n, val + 1));
 else
 	return (-1);
