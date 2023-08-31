8e5369ab37a794673042fa4cd530503e844fdffc
Samira Siham <samirasiham.school2021@gmail.com>
2023-08-30 17:31:10 -0700

    done

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,15 @@
 #include "main.h"
+/*
+ * _strlen_recursion - prints the length of a string
+ * @s : pointer to string
+ * return : integer
+ */
 int _strlen_recursion(char *s)
 {
 	int ntr = 0;
 	if (*s < '\0')
 	{
-		ntr += _strlen_recursion(s+1) +1;
+		ntr += _strlen_recursion(s + 1) + 1;
 	}
 	return (ntr);
 }
