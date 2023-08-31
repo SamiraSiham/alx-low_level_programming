6c947d5fb5c4e8ec14dfe78030eb9d8c312689c6
Samira Siham <samirasiham.school2021@gmail.com>
2023-08-30 17:01:23 -0700

    done

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+int check_prime(int n, int othrn);
+int is_prime_number(int n)
+{
+	return (check_prime(n,2));
+}
+int check_prime(int n , int othrn)
+{
+	if(othrn >= n && n>1)
+		return (1);
+	else if (n%othrn == 0 || n<=1)
+		return (0);
+	else
+		return (check_prime(n,othrn+1));
+}
