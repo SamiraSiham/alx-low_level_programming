5b6e5bf9c1bffa6a9cc550d3fcc658aaf6e29d2a
Samira Siham <samirasiham.school2021@gmail.com>
2023-08-30 17:19:28 -0700

    done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,9 @@
 #include "main.h"
-/*function description*/
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * *s : pointer to the string
+ * Return : void
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,12 @@
 #include "main.h"
+/**
+ * _print_rev_recursion : prints a string
+ * *s : points to a string
+ * return : void
+ */
 void _print_rev_recursion(char *s)
 {
-	if(*s > '\0')
+	if (*s > '\0')
 	{
 		_print_rev_recursion(s+1);
 		_putchar(*s);
