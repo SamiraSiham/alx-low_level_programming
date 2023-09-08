aa0a9713db01c94c90f59710ff9e39ebd45da95a
Samira Siham <samirasiham.school2021@gmail.com>
2023-09-08 14:41:49 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,11 +5,13 @@
  * @b : int
  * Return: pointer to the array initialized or NULL
  */
+
 void *malloc_checked(unsigned int b)
 {
 	int *m = malloc(b);
 
 	if (m == 0)
 		exit(98);
+	
 	return (m);
 }
