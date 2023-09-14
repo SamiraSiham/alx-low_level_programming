4e384fc9abc5d503ca4eba74670845706c337e65
Samira Siham <samirasiham.school2021@gmail.com>
2023-09-13 17:34:39 -0700

    done

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,11 +1,12 @@
-#include "3-calc.c"
+#include "3-calc.h"
 
 /**
- * get_op_func - returns pointer to right op func
- * @s: string operator
- *
- * Return: the right function pointer
+ * get_op_func - function pointer to compute given an operator
+ * @s: the operator given as an input
+ * Return: pointer to the function that will do the op given as
+ * parameter as input
  */
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -14,15 +15,18 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{"NULL", NULL}
+		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
-	while (i < 5)
+	i = 0;
+
+	while (ops[i].op)
 	{
-		if (s && s[0] == ops[i].op[0] && !s[1])
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
+
 	return (NULL);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,58 +1,61 @@
-#include "3-calc.h"
+i#include "3-calc.h"
 
 /**
- *  op_add - adds two integers
- *  @a: the first int
- *  @b: the second int
- *
- *  Return: the sum
+ * op_add - adds two integers
+ * @a: the 1st integer
+ * @b: the second integer
+ * Return: the result of a + b
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- *  op_sub - subtracts two integers
- *  @a: the first int
- *  @b: the second int
- *
- *  Return: the difference
+ * op_sub - substructs two integers
+ * @a: the 1st integer
+ * @b: the second integer
+ * Return: the result of a - b
  */
+
 int op_sub(int a, int b)
 {
-        return (a - b);
-}
-/**
- *  op_mul - multiplies two integers
- *  @a: the first int
- *  @b: the second int
- *
- *  Return: the product
- */
-int op_mul(int a, int b)
-{
-        return (a * b);
-}
-/**
- *  op_div - divides two integers
- *  @a: the first int
- *  @b: the second int
- *
- *  Return: the quotient
- */
-int op_mul(int a, int b)
-{
-        return (a / b);
-}
-/**
- *  op_mod - modulo two integers
- *  @a: the first int
- *  @b: the second int
- *
- *  Return: the remainder
- */
-int op_mod(int a, int b)
-{
-        return (a % b);
+	return (a - b);
 }
 
+/**
+ * op_mul - multiplies two integers
+ * @a: the 1st integer
+ * @b: the second integer
+ * Return: the result of a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - computes a / b
+ * @a: the 1st integer
+ * @b: the second integer
+ * Return: the result of a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - computes a % b
+ * @a: the 1st integer
+ * @b: the second integer
+ * Return: the result of a / b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
