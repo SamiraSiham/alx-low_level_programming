eef9076f54daf3854b3b34c968decb8834e1284a
SamiraSiham <samirasiham.school2021@gmail.com>
2024-02-08 12:27:05 -0800

    binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,7 @@
 
 unsigned int binary_to_uint(const char* b)
 {
-	unsigned int num = 0;
+	unsigned int n = 0;
 
 	if (!b)
 		return (0);
@@ -18,8 +18,8 @@ unsigned int binary_to_uint(const char* b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		num = (num *2) + (*b++ - '0');
+		n = n * 2 + (*b++ - '0');
 	}
-	return (num);
+	return (n);
 }
 
