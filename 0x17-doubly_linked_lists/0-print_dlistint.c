b2b77a817e129a6fcb61dcae455b4d1597509294
SamiraSiham <samirasiham.school2021@gmail.com>
2024-03-21 16:29:52 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_dlistint - print all the elements of dlistint_t list
- * h: pointer to the head of the list
+ * @h: pointer to the head of the list
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
@@ -15,5 +15,5 @@ size_t print_dlistint(const dlistint_t *h)
 		it = it->next;
 		count++;
 	}
-	return count;
+	return (count);
 }
