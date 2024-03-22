ce6ba1dc986d5a9ffdf63aa32e94bb855c26f0b0
SamiraSiham <samirasiham.school2021@gmail.com>
2024-03-21 18:02:21 -0700

    task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,7 +8,7 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
-	dlistint_t *it = head;
+	dlistint_t *it = *head;
 
 	if (!head || !new)
 		return (new ? free(new), NULL : NULL);
