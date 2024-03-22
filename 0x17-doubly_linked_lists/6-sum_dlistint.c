37dcadc3989d2d103ed9037b1adde9cb863fd639
SamiraSiham <samirasiham.school2021@gmail.com>
2024-03-21 18:30:08 -0700

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_dlistint - count the sum of data in doubly linked list
+ * @head: pointer to head of list
+ * Return: sum of values, 0 if list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *it = head;
+	unsigned int sum = 0;
+
+	while(it)
+	{
+		sum += it->n;
+		it = it->next;
+	}
+	return (sum);
+}
