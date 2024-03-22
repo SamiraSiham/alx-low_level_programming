7950d646e900d2fd0e37f2ddfdc51f64f11394b9
SamiraSiham <samirasiham.school2021@gmail.com>
2024-03-21 18:06:18 -0700

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - free doubly linked list
+ * @head: pointer to head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *it;
+	
+	while(it)
+	{
+		it = head;
+		head = head->next;
+		free(it);
+	}
+}
