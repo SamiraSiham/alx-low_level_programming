b1a0050dc54d09d04b5305b58c3348461ceb1d5b
SamiraSiham <samirasiham.school2021@gmail.com>
2024-04-14 18:16:26 -0700

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,68 +1,27 @@
 #include "hash_tables.h"
 /**
- * make_hash_node - create new hash node
- * @key: node key
- * @value: node value
- * Return: new node, or NULL
- */
-hash_node_t *make_hash_node(const char *key, const char *value)
-{
-	hash_node_t *node;
-
-	node = malloc(sizeof(hash_node_t));
-	if (!node)
-		return (NULL);
-	node->key = strdup(key);
-	if (!node->key)
-	{
-		free(node);
-		return (NULL);
-	}
-	node->value = strdup(value);
-	if (!node->value)
-	{
-		free(node->key);
-		free(node);
-		return (NULL);
-	}
-	node->next = NULL;
-	return (node);
-}
-/**
- * hash_table_set - set a key for a hash table value
+ * hash_table_get - get a value from a hash table key
  * @ht: hash table
  * @key: key
- * @value: value
- * Return: 1 on success, 0 otherwise
+ * Return: value on success, NULL otherwise
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
-	hash_node_t *node, *tmp;
-	char *new_value;
+	hash_node_t *tmp;
 
 	if (!ht || !ht->array || ht->size == 0 ||
-	    !key || strlen(key) == 0 || !value)
-		return (0);
+	    !key || strlen(key) == 0)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 	while (tmp)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
-			new_value = strdup(value);
-			if (!new_value)
-				return (0);
-			free(tmp->value);
-			tmp->value = new_value;
-			return (1);
+			return (tmp->value);
 		}
 		tmp = tmp->next;
 	}
-	node = make_hash_node(key, value);
-	if (!node)
-		return (0);
-	node->next = ht->array[index];
-	ht->array[index] = node;
-	return (1);
+	return (NULL);
 }
